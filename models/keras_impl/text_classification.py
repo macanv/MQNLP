9c42dcf76a28998ce228dcc19a2fe0c388c198df
macan <macan1015>
2017-12-08 18:05:43 +0800

    load thu data

--- models/keras_impl/text_classification.py
+++ models/keras_impl/text_classification.py
@@ -4,3 +4,104 @@ from keras.models import Sequential
 from keras.layers import Dense, Conv1D, Activation, Dropout, GlobalMaxPooling1D, Embedding
 from keras.preprocessing import sequence
 
+class TextCNN():
+
+    def __init__(self, batch_size=32, embedding_mat=None, embedding_dims=200,
+                 filters=200, regions_size=3, hidden_dims=3, epochs=5, keep_dropout_prob=0.5,
+                 max_features=10000, maxlen=400, strides=1, padding='valid', actiovation='relu',
+                 num_category=2, last_activation='sigmoid',
+                 optimizer='adam', loss='binary_crossentropy', metrics=['accuracy']):
+        """
+        卷积神经网络用于文本分类
+        :param batch_size: 
+        :param max_features: 最大特征个数 max features size
+        :param maxlen: 
+        :param embedding_mat: pre_train word2vec or glove 
+        :param embedding_dims:  word vector dims
+        :param filters: num of filter
+        :param region_size: each filter height
+        :param hidden_dims: FC hidden dims
+        :param epochs: train epoch size 
+        """
+        self.batch_size = batch_size
+        if embedding_mat is not None:
+            self.embedding_mat = embedding_mat
+            if embedding_mat.shape[1] != embedding_dims:
+                raise ValueError('embedding_mat dims not equal embedding_dims')
+        self.embedding_dims = embedding_dims
+        self.filters = filters
+        self.regions_size = regions_size
+        self.hidden_dims = hidden_dims
+        self.epochs = epochs
+        self.keep_dropout_prop = keep_dropout_prob
+        self.max_features = max_features
+        self.maxlen = maxlen
+        self.strides = strides
+        self.padding = padding
+        self.actiovation = actiovation
+        self.num_category = num_category
+        self.last_activation = last_activation
+        self.optimizer = optimizer
+        self.loss =loss
+        self.metrics = metrics
+
+        self.model = Sequential()
+        self.__build_neuro_network()
+
+    def __build_neuro_network(self):
+        """
+        网络搭建
+        :param max_features: 
+        :param maxlen: 
+        :param strides: 
+        :param padding: 
+        :param actiovation: 
+        :param num_category: 
+        :param last_activation: 
+        :return: 
+        """
+        # 1.embedding layer
+
+        self.model.add(Embedding(input_dim=self.max_features,
+                                 output_dim=self.embedding_dims,
+                                 input_length=self.maxlen))
+        self.model.add(Dropout(self.keep_dropout_prop))
+
+        # 2.convilution layer
+        self.model.add(Conv1D(self.filters, self.regions_size, strides=self.strides,
+                              padding=self.padding, activation=self.actiovation))
+
+        # 3. pooling
+        self.model.add(GlobalMaxPooling1D())
+
+        # 4. FC
+        self.model.add(Dense(self.hidden_dims))
+        self.model.add(Dropout(self.keep_dropout_prop))
+        self.model.add(Activation('relu'))
+
+        self.model.add(Dense(self.num_category))
+        self.model.add(Activation(self.last_activation))
+
+    def train(self, x_train, y_train, x_test, y_test):
+        """
+        模型训练与评价
+        :param optimizer: 
+        :param loss: 
+        :param merics: 
+        :return: 
+        """
+        self.model.compile(optimizer=self.optimizer, loss=self.loss, metrics=self.metrics)
+        self.model.fit(x_train, y_train, batch_size=self.batch_size, epochs=self.epochs)
+        self.model.evaluate(x_test, y_test, batch_size=self.batch_size)
+
+if __name__=='__main__':
+    from keras.datasets import imdb
+    from keras.preprocessing import sequence
+
+    (x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=5000, maxlen=400)
+    x_train = sequence.pad_sequences(x_train, 400)
+    x_test = sequence.pad_sequences(x_test, 400)
+
+    clf = TextCNN(epochs=3, batch_size=32, embedding_dims=50, filters=250, regions_size=3, hidden_dims=250, num_category=1, last_activation='sigmoid')
+    clf.train(x_train, y_train, x_test, y_test)
+
--- utils/thu.py
+++ utils/thu.py
@@ -2,8 +2,15 @@
 import os
 import re
 import numpy as np
+import codecs
+from tensorflow.contrib import learn
 from sklearn.utils import shuffle
-def load_data_from_file(file_path, num_words, skip_top=0, maxlen=None, seed=113):
+import pickle
+import collections
+
+local_path = os.path.join(os.getcwd(), '..')
+
+def load_data_from_file(file_path, num_words, skip_top=0, maxlen=None, minlen=0, seed=113, delLine=False):
     """
     load THU text classification datasets
     original data URL : http://thuctc.thunlp.org/
@@ -12,15 +19,17 @@ def load_data_from_file(file_path, num_words, skip_top=0, maxlen=None, seed=113)
     :param skip_top: skip the top N frequently occirring words
     :param maxlen:  max sequences length ,if input sequences length is longer than maxlen, 
                     it will be filtered out
+    :param minlen: 
     :param seed: random sees for sample shuffling
+    :param delLine: 删除大于maxlen 的样本
     :return: 
    """
-    category = ['体育', '股票', '财经']
+    category = ['体育', '股票', '科技']
     lens = len(category)
 
     if os.path.isfile(file_path):
         corpus = []
-        with open(file_path, 'r') as fd:
+        with codecs.open(file_path, 'r', 'utf-8') as fd:
            for line in fd:
                corpus.append(line)
         corpus = shuffle(corpus)
@@ -28,15 +37,26 @@ def load_data_from_file(file_path, num_words, skip_top=0, maxlen=None, seed=113)
         input_y = []
         for line in corpus:
             index = line.find(' ')
-            tag = doc[:index]
+            tag = line[:index]
             tag = re.sub('__label__', '', tag)
+
             y_ = np.zeros(lens)
-            i = category.index(tag)
+            try:
+                i = category.index(tag)
+            except ValueError:
+                print(line)
+                continue
             y_[i] = 1
             input_y.append(y_)
 
-            # for maxlen
-            input_x.append(line[index + 1: maxlen + index + 1])
+            # for maxlen and encode text to index
+            input_x.append(line[index + 1:])
+        max_len = max([len(x) for x in input_x])
+        if maxlen is None:
+            maxlen = max_len
+        maxlen = min(max_len, maxlen)
+        vocab_process = learn.preprocessing.VocabularyProcessor(max_document_length=maxlen, min_frequency=minlen)
+        input_x = np.array(list(vocab_process.fit_transform(input_x)))
 
         return [input_x, input_y]
     else:
@@ -57,7 +77,47 @@ def load_data(file_path):
         data = pickle.load(fd)
     return data
 
-if __name__ == '__main__':
-    file_path = ''
 
-    input_x, input_y = load_data_from_file(file_path, num_words, skip_top, maxlen, seed)
+def build_datasets(words, max_fequence=10000):
+    """
+    :param corpus: 
+    :return: 
+    """
+    count = [['UNK', -1]]
+    count.extend(collections.Counter(words).most_common(max_fequence - 1))
+    dictionary = {}
+    for word, _ in count:
+        dictionary[word] = len(dictionary) # word index
+    data = []
+    unk_count = 0
+    for word in words:
+        if word in dictionary:
+            index = dictionary[word]
+        else:
+            index = 0
+            unk_count += 1
+        data.append(index)
+    count[0][1] = unk_count
+    reverse_dictionary = dict(zip(dictionary.values(), dictionary.keys()))
+
+    return data, count, dictionary, reverse_dictionary
+
+def word_to_id(texts, max_feq, min_feq):
+    """
+    
+    :param texts: 
+    :param max_feq: 
+    :param min_feq: 
+    :return: 
+    """
+
+if __name__ == '__main__':
+    resource_path = os.path.join(local_path, 'resources')
+    file_path = os.path.join(resource_path, 'thu_data_3class_1w')
+
+    input_x, input_y = load_data_from_file(file_path, num_words=10000, maxlen=400)
+    with open(os.path.join(resource_path, 'thu_data_3class_1w.pkl'), 'wb') as fd:
+        pickle.dump([input_x, input_y], fd)
+
+
+
