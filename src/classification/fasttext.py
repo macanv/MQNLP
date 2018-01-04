beb1184360fffe420aee412fd6beca2e5cff1b24
macan <canma.love@gmail.com>
2018-01-04 17:35:04 +0800

    检查fasttext 的训练.py文件

---
+++ src/classification/fasttext.py
@@ -0,0 +1,80 @@
+# coding=utf-8
+import tensorflow as tf
+
+# from src.basicModel import basicModel
+
+
+class fasttext(object):
+    def __init__(self, num_tags, sequence_length, vocab_size, embedding_dim, num_sampled,
+                 l2_reg_lambda):
+        """
+
+        :param num_classes:
+        :param sentence_length:
+        :param vovab_size:
+        :param embedding_dims:
+        """
+        self.num_tags = num_tags
+        self.sentence_length = sequence_length
+        self.vocab_size = vocab_size
+        self.embedding_dims = embedding_dim
+        self.l2_reg_lambda = l2_reg_lambda
+        self.num_sampled = num_sampled
+
+        self.input_x = tf.placeholder(tf.int32, [None, self.sentence_length], name='input_x')
+        self.input_y = tf.placeholder(tf.int32, [None, self.num_tags], name='input_y')
+
+        self.loss = tf.constant(0.0)
+
+        self.loss = tf.constant(0.0)
+        self.accuracy = tf.constant(0.0)
+        self.num_correct = tf.constant(0.0)
+
+        self.network()
+
+    def network(self):
+        """
+
+        :return:
+        """
+        # 1. embedding layer
+        with tf.device('/cpu:0'), tf.name_scope('embedding'):
+            self.Embedding = tf.Variable(tf.random_uniform([self.vocab_size, self.embedding_dims],
+                                                           -1., 1.,dtype=tf.float32), name='Embedding')
+            self.embedding_chars = tf.nn.embedding_lookup(self.Embedding, self.input_x)
+
+        # 2. skip gram
+        with tf.name_scope('skip-gram'):
+            self.sentence_embedd = tf.reduce_mean(self.embedding_chars, axis=1) #[None,self.enbedding_dim]
+
+            #FC
+            self.W = tf.get_variable('W', [self.embedding_dims, self.num_tags])
+            self.b = tf.get_variable('b', [self.num_tags])
+
+            self.logits = tf.matmul(self.sentence_embedd, self.W) + self.b
+
+        with tf.name_scope('loss'):
+            # classes = tf.reshape(self.num_classes, [-1])
+            # classes = tf.expand_dims(classes, 1)
+            # self.loss = tf.reduce_mean(tf.nn.nce_loss(weights=tf.transpose(self.W),
+            #                                       biases=self.b,
+            #                                       labels=classes,
+            #                                       inputs=self.sentence_embedd,
+            #                                       num_sampled=self.num_sampled,
+            #                                       num_classes=self.num_classes,
+            #                                       partition_strategy='div'))
+            print('-----logits-----',self.logits)
+            print('-----input_y----', self.input_y)
+            self.loss = tf.nn.softmax_cross_entropy_with_logits(logits=self.logits,
+                                                                labels=self.input_y)
+            self.loss = tf.reduce_mean(self.loss)
+            l2_loss = tf.nn.l2_loss(self.W)
+            l2_loss += tf.nn.l2_loss(self.b)
+            self.loss += l2_loss * self.l2_reg_lambda
+
+        with tf.name_scope('accuracy'):
+            self.correct = tf.equal(tf.argmax(self.logits, axis=1), tf.argmax(self.input_y, axis=1))
+            self.accuracy = tf.reduce_mean(tf.cast(self.correct, dtype=tf.float32), name='accuracy')
+
+        with tf.name_scope('num_correct'):
+            self.num_correct = tf.reduce_sum(tf.cast(self.correct, dtype=tf.float32), name='num_correct')
--- src/classification/main.py
+++ src/classification/main.py
@@ -9,11 +9,12 @@ import datetime
 import os
 import pickle
 import sys
-sys.path.append("C:\workspace\python\MQNLP")
+sys.path.append("../..")
 from src.classification.data_helper import batch_manager, load_data, batch_iter
 from src.classification.CNNs import CNNClassification
 from src.classification.CNNRNNs import CNNRNNsClassification
 from src.classification.RNNs import RNNsClassification
+from src.classification.fasttext import fasttext
 
 def train_cnns():
     """
@@ -424,7 +425,6 @@ def train_rnn():
         print("{}={}".format(attr.upper(), value))
     print("")
 
-
     input_x, input_y, vocab_proccesser = load_data(FLAGS.train_path, FLAGS.sequence_length)
     train_batches = batch_iter(list(zip(input_x, input_y)), FLAGS.batch_size, FLAGS.num_epochs)
 
@@ -561,13 +561,175 @@ def train_rnn():
 
             print('\nBset dev at {}, accuray {:g}'.format(best_step, best_acc))
 
+def train_fasttext():
+    # Data loading params
+    tf.flags.DEFINE_float("dev_sample_percentage", .1, "Percentage of the training data to use for validation")
+    tf.flags.DEFINE_string("train_path", "thu_train", "Data source.")
+    tf.flags.DEFINE_string("dev_path", "thu_dev", "Data source.")
+    tf.flags.DEFINE_integer('sequence_length', 400, 'length of each sequence')
+    tf.flags.DEFINE_integer("num_tags", 3, "number classes of datasets.")
+
+    # Model Hyperparameters
+    tf.flags.DEFINE_integer("embedding_dim", 200, "Dimensionality of character embedding (default: 128)")
+    tf.flags.DEFINE_float("dropout_keep_prob", 0.5, "Dropout keep probability (default: 0.5)")
+    tf.flags.DEFINE_float("l2_reg_lambda", 0.0, "L2 regularization lambda (default: 0.0)")
+    tf.app.flags.DEFINE_integer("num_sampled", 50, "number of noise sampling")
+
+    # Training parameters
+    tf.flags.DEFINE_integer("batch_size", 32, "Batch Size (default: 64)")
+    tf.flags.DEFINE_integer("num_epochs", 100, "Number of training epochs (default: 200)")
+    tf.flags.DEFINE_integer("evaluate_every", 100, "Evaluate model on dev set after this many steps (default: 100)")
+    tf.flags.DEFINE_integer("checkpoint_every", 100, "Save model after this many steps (default: 100)")
+    tf.flags.DEFINE_integer("num_checkpoints", 5, "Number of checkpoints to store (default: 5)")
+    # Misc Parameters
+    tf.flags.DEFINE_boolean("allow_soft_placement", True, "Allow device soft device placement")
+    tf.flags.DEFINE_boolean("log_device_placement", False, "Log placement of ops on devices")
+
+    FLAGS = tf.flags.FLAGS
+    FLAGS._parse_flags()
+    print('\nParameters')
+    for attr, value in sorted(FLAGS.__flags.items()):
+        print("{}={}".format(attr.upper(), value))
+    print("")
+
+    input_x, input_y, vocab_proccesser = load_data(FLAGS.train_path, FLAGS.sequence_length)
+    train_batches = batch_iter(list(zip(input_x, input_y)), FLAGS.batch_size, FLAGS.num_epochs)
+
+    dev_x, dev_y, _ = load_data(FLAGS.dev_path, FLAGS.sequence_length)
+
+    with tf.Graph().as_default():
+        session_conf = tf.ConfigProto(
+            allow_soft_placement=FLAGS.allow_soft_placement,
+            log_device_placement=FLAGS.log_device_placement)
+        sess = tf.Session(config=session_conf)
+        with sess.as_default():
+            # 构建cnn 节点
+            ft = fasttext(
+                num_tags=FLAGS.num_tags,
+                sequence_length=FLAGS.sequence_length,
+                vocab_size=len(vocab_proccesser.vocabulary_),
+                embedding_dim=FLAGS.embedding_dim,
+                num_sampled=FLAGS.num_sampled,
+                l2_reg_lambda=FLAGS.l2_reg_lambda)
+
+            # Define Training procedure
+            global_step = tf.Variable(0, name="global_step", trainable=False)
+            # 优化算法
+            optimizer = tf.train.AdamOptimizer(1e-3)
+            grads_and_vars = optimizer.compute_gradients(ft.loss)
+
+            train_op = optimizer.apply_gradients(grads_and_vars, global_step=global_step)
+
+            # Keep track of gradient values and sparsity (optional)
+            grad_summaries = []
+            for g, v in grads_and_vars:
+                if g is not None:
+                    grad_hist_summary = tf.summary.histogram("{}/grad/hist".format(v.name), g)
+                    sparsity_summary = tf.summary.scalar("{}/grad/sparsity".format(v.name),
+                                                         tf.nn.zero_fraction(g))
+                    grad_summaries.append(grad_hist_summary)
+                    grad_summaries.append(sparsity_summary)
+            grad_summaries_merged = tf.summary.merge(grad_summaries)
+
+            # Output directory for models and summaries
+            timestamp = str(int(time.time()))
+            out_dir = os.path.abspath(os.path.join(os.path.curdir, "runs_fasttext", timestamp))
+            print("Writing to {}\n".format(out_dir))
+
+            # Summaries for loss and accuracy
+            loss_summary = tf.summary.scalar("loss", ft.loss)
+            acc_summary = tf.summary.scalar("accuracy", ft.accuracy)
+
+            # Train Summaries
+            train_summary_op = tf.summary.merge([loss_summary, acc_summary, grad_summaries_merged])
+            train_summary_dir = os.path.join(out_dir, "summaries", "train")
+            train_summary_writer = tf.summary.FileWriter(train_summary_dir, sess.graph)
+
+            # Dev summaries
+            dev_summary_op = tf.summary.merge([loss_summary, acc_summary])
+            dev_summary_dir = os.path.join(out_dir, "summaries", "dev")
+            dev_summary_writer = tf.summary.FileWriter(dev_summary_dir, sess.graph)
+
+            # Checkpoint directory. Tensorflow assumes this directory already exists so we need to create it
+            checkpoint_dir = os.path.abspath(os.path.join(out_dir, "checkpoints"))
+            checkpoint_prefix = os.path.join(checkpoint_dir, "model")
+            if not os.path.exists(checkpoint_dir):
+                os.makedirs(checkpoint_dir)
+            saver = tf.train.Saver(tf.global_variables(), max_to_keep=FLAGS.num_checkpoints)
+
+            # Write vocabulary
+            vocab_proccesser.save(os.path.join(out_dir, "vocab"))
+
+            # Initialize all variables
+            sess.run(tf.global_variables_initializer())
+
+            def train_step(x_batch, y_batch):
+                """
+                A single training step
+                """
+                feed_dict = {
+                    ft.input_x: x_batch,
+                    ft.input_y: y_batch,
+                }
+                # 执行 节点操作
+                _, step, summaries, loss, accuracy, num_correct = sess.run(
+                    [train_op, global_step, train_summary_op, ft.loss, ft.accuracy, ft.num_correct],
+                    feed_dict)
+
+                time_str = datetime.datetime.now().isoformat()
+                if step % 20 == 0:
+                    print("{}: step {}, loss {:g}, acc {:g}, predicted {:g}".format(time_str, step, loss, accuracy,
+                                                                                    predicted))
+                train_summary_writer.add_summary(summaries, step)
+
+            def dev_step(x_batch, y_batch, writer=None):
+                """
+                Evaluates model on a dev set
+                """
+                feed_dict = {
+                    ft.input_x: x_batch,
+                    ft.input_y: y_batch,
+                }
+                step, summaries, loss, accuracy, num_correct = sess.run(
+                    [global_step, dev_summary_op, ft.loss, ft.accuracy, ft.num_correct],
+                    feed_dict)
+                time_str = datetime.datetime.now().isoformat()
+                print("{}: step {}, loss {:g}, acc {:g}, predicted {:g}".format(time_str, step, loss, accuracy,
+                                                                                predicted))
+                if writer:
+                    writer.add_summary(summaries, step)
+                return loss, accuracy, num_correct
+
+            # Training loop. For each batch...
+            best_acc = 0.0
+            best_step = 0
+            predicted = 0.0
+            for batch in train_batches:
+                x_batch, y_batch = zip(*batch)
+                train_step(x_batch, y_batch)
+                # 更新全局步数
+                current_step = tf.train.global_step(sess, global_step)
+                # 计算评估结果
+                if current_step % FLAGS.evaluate_every == 0:
+                    print("\nEvaluation:")
+                    loss_, accuracy_, predicted_ = dev_step(dev_x, dev_y, writer=dev_summary_writer)
+                    if accuracy_ > best_acc:
+                        best_acc = accuracy_
+                        predicted = predicted_
+                        best_step = current_step
+                        # 保存模型计算结果
+                        path = saver.save(sess, checkpoint_prefix, global_step=current_step)
+                        print("Saved model checkpoint to {}\n".format(path))
+                        print("")
+
+            print('\nBset dev at {}, accuray {:g}, predicted {:g}'.format(best_step, best_acc, predicted))
 def main(_):
     # CNNs methods3
 
     # train_cnns()
     # train_cnnrnn()
-    train_rnn()
-
+    # train_rnn()
+    train_fasttext()
 if __name__ == '__main__':
     tf.app.run()
 
